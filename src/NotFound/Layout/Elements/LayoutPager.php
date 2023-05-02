a202a32243b0002fa7e84e7d52b07094c0c51b36
Rene <rene@notfound.nl>
2023-05-02 14:28:33 +0200

    feat: items per page

--- src/NotFound/Layout/Elements/LayoutPager.php
+++ src/NotFound/Layout/Elements/LayoutPager.php
@@ -4,10 +4,11 @@ namespace NotFound\Layout\Elements;
 
 class LayoutPager extends AbstractLayout
 {
-    public function __construct(int $totalItems)
+    public function __construct(int $totalItems, ?int $itemsPerPage = null)
     {
         parent::__construct(type: 'Pager');
 
+        $this->properties->itemsPerPage = $itemsPerPage ?? 25;
         $this->properties->totalItems = $totalItems;
     }
 }
