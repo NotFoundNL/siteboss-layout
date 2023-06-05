e35087ee5f86aad058ce6998e02526536b039e50
Rene <rene@notfound.nl>
2023-06-05 15:47:35 +0200

    fix: remove type from AbstractValidation

--- src/NotFound/Layout/Validations/AbstractValidation.php
+++ src/NotFound/Layout/Validations/AbstractValidation.php
@@ -2,12 +2,10 @@
 
 namespace NotFound\Layout\Validations;
 
-use App\Services\Assets\Components\AbstractComponent;
-
 abstract class AbstractValidation
 {
     public function __construct(
-        protected AbstractComponent $input,
+        protected $input,
     ) {
     }
 
