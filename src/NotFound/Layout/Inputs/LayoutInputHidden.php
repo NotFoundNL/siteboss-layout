aebcba8226ebe3d142714d07f6aa487eec85e3dd
keeama13 <xander@schuurmannen.nl>
2024-01-11 17:07:11 +0100

    feat: input hidden

---
+++ src/NotFound/Layout/Inputs/LayoutInputHidden.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NotFound\Layout\Inputs;
+
+class LayoutInputHidden extends LayoutInputText
+{
+    protected string $type = 'InputHidden';
+
+    protected string $value = '';
+}
